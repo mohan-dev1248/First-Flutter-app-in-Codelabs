c801cf079fcfff33bbd2639f9153a9a000b84c42
Mohan-eng-mtap <mohanakrishna.v@mtap.in>
2021-05-31 19:43:18 +0530

    Update Part 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: RandomWords(),
     );
   }
@@ -40,6 +23,7 @@ class RandomWords extends StatefulWidget{
 class _RandomWordsState extends State<RandomWords>{
 
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _bigFont = const TextStyle(fontSize: 18);
 
   Widget _buildSuggestions() => ListView.builder(
@@ -69,11 +53,25 @@ class _RandomWordsState extends State<RandomWords>{
   );
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _bigFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          } else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -82,8 +80,35 @@ class _RandomWordsState extends State<RandomWords>{
     return Scaffold (                     // Add from here...
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context){
+            final tiles = _saved.map((pair) => ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _bigFont,
+                ),
+            ));
+            final divided = tiles.isNotEmpty
+                ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+                : <Widget>[];
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          }
+      )
     );
   }
 }
